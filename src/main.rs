89430be5c52456fdef093420e015665eea009842
neiman <eyalron33@gmail.com>
2024-01-01 09:03:26 +0100

    working private-public key generator for babyjubjub

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,30 @@ fn main() {
     // Compute the corresponding public key
     let public_key: Point = private_key.public();
 
-    // Print the private key and public key
-    println!("Private Key: {:?}", private_key.scalar_key());
-    println!("Public Key: {:?}", public_key);
+    // extract the arrays form the keys
+    let private_key_something_bigint = private_key.scalar_key().0;
+    let private_key_arr : [u64; 4] = private_key.scalar_key().0.0;
+    let public_key_x_arr: [u64; 4] = public_key.x.0.0;
+    let public_key_y_arr: [u64; 4] = public_key.y.0.0;
+
+    let private_key_something_bigint_hex = format!("{private_key_something_bigint:X}");
+
+    println!("private key something: {:?}", private_key_something_bigint_hex);
+
+    print_u64_array(&private_key_arr);
+
+    print!("x: ");
+    print_u64_array(&public_key_x_arr);
+
+    print!("y: ");
+    print_u64_array(&public_key_y_arr);
 }
+
+fn print_u64_array(arr: &[u64]) {
+    for &element in arr {
+        print!("{}", element);
+    }
+
+    println!("");
+}
+
