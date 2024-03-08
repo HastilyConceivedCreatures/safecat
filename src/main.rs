b2db346650c3faf0145b3d185a7f3362c86f5229
neiman <eyalron33@gmail.com>
2024-03-08 15:37:54 +0100

    Fixed indentation in main.rs. Modified the 'show' command to be 'show-keys', so that later we can add 'show-certs' for showing certificates

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,9 @@ fn main() {
         Some(("generate", _)) => {
             generate("priv.key")
         },
-        Some(("show", sub_matches)) => {
+        Some(("show-keys", sub_matches)) => {
             let format = sub_matches.get_one::<String>("format").expect("defaulted in clap");
-            show(format.to_string())
+            show_keys(format.to_string())
         },
         Some(("sign", sub_matches)) => {
             let msg = sub_matches.get_one::<String>("MESSAGE").expect("required");
@@ -45,7 +45,7 @@ fn main() {
             verify_signature(msg, signature, private_key, hash)
         },
         Some((_, _)) => {
-            print!("unknown command, usage 'safecat <generate|show|sign|verify>'")
+            print!("unknown command, usage 'safecat <generate|show-keys|sign|verify>'")
         },   
         None => todo!()
         }
@@ -63,7 +63,7 @@ fn cli() -> Command {
                 .about("Generates a private key") 
         )
         .subcommand(
-            Command::new("show")
+            Command::new("show-keys")
                 .about("Shows keys")
                 .arg(arg!(--"format" <FORMAT>)
                      .value_parser(["detailed", "hex"])
@@ -129,7 +129,7 @@ fn generate(privatekey_filename: &str) {
 }
 
 // Displays private and public keys based on the specified output format.
-fn show(output_format: String) {
+fn show_keys(output_format: String) {
     // Check if private key file exists
     if !file_exists("priv.key") {
         println!("No key was generated yet.");
@@ -164,67 +164,67 @@ fn show(output_format: String) {
 
 // Signs a message using BabyJubJub based on the specified hash algorithm and output format.
 fn sign(message_to_sign_string: String, hash_algorithm: String, output_format: String) {
-            if hash_algorithm == "poseidon"
-                && message_to_sign_string.len() > consts::MAX_POSEIDON_PERMUTATION_LEN * consts::PACKED_BYTE_LEN
-            {
-                io_utils::bad_command("message_too_long");
-            }
+    if hash_algorithm == "poseidon"
+        && message_to_sign_string.len() > consts::MAX_POSEIDON_PERMUTATION_LEN * consts::PACKED_BYTE_LEN
+    {
+        io_utils::bad_command("message_too_long");
+    }
 
-            // Check if private key file exists
-            if !file_exists("priv.key") {
-                println!("No key was generated yet.");
-                return;
-            }
+    // Check if private key file exists
+    if !file_exists("priv.key") {
+        println!("No key was generated yet.");
+        return;
+    }
 
-            let private_key = io_utils::load_private_key("priv.key").unwrap();
+    let private_key = io_utils::load_private_key("priv.key").unwrap();
 
-            let hash_fq = calculate_hash_fq(&message_to_sign_string, &hash_algorithm);
+    let hash_fq = calculate_hash_fq(&message_to_sign_string, &hash_algorithm);
 
-            // Print the hash
-            println!("message Hash: {}", cast::fq_to_dec_string(&hash_fq));
+    // Print the hash
+    println!("message Hash: {}", cast::fq_to_dec_string(&hash_fq));
 
-            // Sign the message
-            let signature = private_key.sign(hash_fq).expect("Failed to sign message");
+    // Sign the message
+    let signature = private_key.sign(hash_fq).expect("Failed to sign message");
 
-            if output_format == "detailed" {
-                // Print signature
-                println!("Signature: R.X: {}", cast::fq_to_dec_string(&signature.r_b8.x));
-                println!("Signature: R.Y: {}", cast::fq_to_dec_string(&signature.r_b8.y));
-                println!("Signature: S: {}", cast::fr_to_dec_string(&signature.s));
-            } else if output_format == "hex" {
-                // change signature variables to hex
-                let signature_x_hex = cast::fq_to_hex_string(&signature.r_b8.x);
-                let signature_y_hex = cast::fq_to_hex_string(&signature.r_b8.y);
-                let signature_s_hex = cast::fr_to_hex_string(&signature.s);
+    if output_format == "detailed" {
+        // Print signature
+        println!("Signature: R.X: {}", cast::fq_to_dec_string(&signature.r_b8.x));
+        println!("Signature: R.Y: {}", cast::fq_to_dec_string(&signature.r_b8.y));
+        println!("Signature: S: {}", cast::fr_to_dec_string(&signature.s));
+    } else if output_format == "hex" {
+        // change signature variables to hex
+        let signature_x_hex = cast::fq_to_hex_string(&signature.r_b8.x);
+        let signature_y_hex = cast::fq_to_hex_string(&signature.r_b8.y);
+        let signature_s_hex = cast::fr_to_hex_string(&signature.s);
 
-                println!(
-                    "Signature: {}{}{}",
-                    signature_x_hex, signature_y_hex, signature_s_hex
-                );
-            }
+        println!(
+            "Signature: {}{}{}",
+            signature_x_hex, signature_y_hex, signature_s_hex
+        );
+    }
 }
 
 // Verifies a message signature using BabyJubJub based on the specified hash algorithm.
 fn verify_signature(message_to_verify_string: String, signature_string: String, public_key_hex_string: String , hash_algorithm: String) {
-            println!("message_to_verify_string: {}", message_to_verify_string);
-            println!("public_key_hex_string: {}", public_key_hex_string);
-            println!("signature_string: {}", signature_string);
+    println!("message_to_verify_string: {}", message_to_verify_string);
+    println!("public_key_hex_string: {}", public_key_hex_string);
+    println!("signature_string: {}", signature_string);
 
-            if  hash_algorithm == "poseidon" &&
-                message_to_verify_string.len() > consts::MAX_POSEIDON_PERMUTATION_LEN * consts::PACKED_BYTE_LEN
-            {
-                io_utils::bad_command("message_too_long");
-            }
+    if  hash_algorithm == "poseidon" &&
+        message_to_verify_string.len() > consts::MAX_POSEIDON_PERMUTATION_LEN * consts::PACKED_BYTE_LEN
+    {
+        io_utils::bad_command("message_too_long");
+    }
 
-            let hash_fq = calculate_hash_fq(&message_to_verify_string, &hash_algorithm);
+    let hash_fq = calculate_hash_fq(&message_to_verify_string, &hash_algorithm);
 
-            // Create PublicKey and signature objects
-            let public_key = cast::public_key_from_str(&public_key_hex_string).unwrap();
-            let signature = cast::signature_from_str(&signature_string);
+    // Create PublicKey and signature objects
+    let public_key = cast::public_key_from_str(&public_key_hex_string).unwrap();
+    let signature = cast::signature_from_str(&signature_string);
 
-            let correct = verify(public_key, signature, hash_fq);
+    let correct = verify(public_key, signature, hash_fq);
 
-            println!("signature is {}", correct);
+    println!("signature is {}", correct);
 }
 
 // Calculateד hash_fq based on hash_algorithm
